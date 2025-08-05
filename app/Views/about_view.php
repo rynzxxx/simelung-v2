0e57240ff2642124d93f7bf5dcecc6bad0981445
rynzxxx <kakeksalto29@gmail.com>
2025-08-05 23:40:29 +0700

    feat: Menambahkan Halaman About

---
+++ app/Views/about_view.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman About</title>
+</head>
+
+<body>
+    <h1>Tentang Kami</h1>
+    <p>ini adalah proyek pertama codeigniter 4 saya yang terhubung dengan github</p>
+    <p>website ini dibuat oleh Rian Hidayat</p>
+</body>
+
+</html>
